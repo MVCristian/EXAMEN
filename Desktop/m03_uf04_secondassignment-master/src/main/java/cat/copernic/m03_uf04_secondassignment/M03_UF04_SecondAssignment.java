cbd826d248e93bd880e820d63f4635897f13cd74
Cristian_21 <mvcristian13@gmail.com>
2020-10-22 15:53:54 +0200

    FINALIZADO

--- Desktop/m03_uf04_secondassignment-master/src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
+++ Desktop/m03_uf04_secondassignment-master/src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
@@ -11,14 +11,25 @@ package cat.copernic.m03_uf04_secondassignment;
  */
 public class M03_UF04_SecondAssignment {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Dispositivos dispositivo1 = new Ordenador("HP", "Ordenador de sobremesa de 17' con un i7 9500K y 12GB de RAM", 599.99);
         Dispositivos dispositivo2 = new TerminalDeVenta("a", "Putno de venta con NFC incluido", 99.99);
         Dispositivos dispositivo3 = new Tablet("iPad Air", "Tamaño de 9,7' y cuenta con una resolución de 2048 X 1536.", 650.99);
         Dispositivos dispositivo4 = new Portatil("Asus", "Portátil de 17' con un Intel Celeron y 4GB de RAM", 349.99);
         
+        dispositivo4.Reset();
+        dispositivo3.Reset();
+        //Aquí no dejará
+        try {
+            dispositivo2.Reset();
+        } catch(Exception error){
+            System.out.println("Error no se puede realizar esta acción");
+        }
         
         System.out.println(dispositivo1);
+        dispositivo2.conecta();
+        dispositivo2.desconecta();
     }
+    
     
 }
